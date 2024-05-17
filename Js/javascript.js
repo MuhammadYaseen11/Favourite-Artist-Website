2464b644f979fdf9f9babb326babf3615937b7a8
YaseenStu <111099331+MuhammadYaseen11@users.noreply.github.com>
2024-05-17 16:35:59 +0000

    update

--- Js/javascript.js
+++ Js/javascript.js
@@ -1,32 +1,52 @@
-document.getElementById('submit').addEventListener('click', (e) => {
-        e.preventDefault()
-        const fname = document.getElementById('fname')
-        const email = document.getElementById('email')
-        re_name = /[a-zA-Z_\s\-\.]{6,32}/
-        re_email = /([a-zA-Z_\d\.\-]+)@([a-zA-Z_\.\-]+)\.[a-zA-Z]{2,5}/
-        if (!re_name.test(fname) || !re_email.test(email)) {
-            alert('Please try again')
-            location.reload()
-        }
-        const data = {
-            "name": fname,
-            "email": email
-        }
-        fetch("https://https://mudfoot.doc.stu.mmu.ac.uk/node/api/mailinglist", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        })
-            .then(res => {
-                if (res.status === 200)
-                    return res.json()
-                else if (res.status === 400)
-                    throw ' "email" must be a valid email '
+document.addEventListener('DOMContentLoaded', function () {
+    const favoriteButtons = document.querySelectorAll('.favorite-btn');
+    const favoriteSongsList = document.getElementById('favorite-songs-list');
 
-                else
-                    throw res.statusText
-            })
-            
-})
+    favoriteButtons.forEach(button => {
+        const songId = button.getAttribute('data-song-id');
+        const songTitle = button.getAttribute('data-song-title');
+        const iframeHTML = button.previousElementSibling.outerHTML;
+
+        if (localStorage.getItem(songId)) {
+            button.querySelector('img').src = '../images/favorite-checked.png';
+            button.querySelector('img').alt = 'Remove from Favorites';
+        } else {
+            button.querySelector('img').src = '../images/favorite-unchecked.png';
+            button.querySelector('img').alt = 'Add to Favorites';
+        }
+
+        button.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            if (localStorage.getItem(songId)) {
+                localStorage.removeItem(songId);
+                button.querySelector('img').src = '../images/favorite-unchecked.png';
+                button.querySelector('img').alt = 'Add to Favorites';
+            } else {
+                localStorage.setItem(songId, iframeHTML);
+                button.querySelector('img').src = '../images/favorite-checked.png';
+                button.querySelector('img').alt = 'Remove from Favorites';
+            }
+
+            updateFavoritesList();
+        });
+    });
+
+    function updateFavoritesList() {
+        if (favoriteSongsList) {
+            favoriteSongsList.innerHTML = '';
+
+            for (let i = 0; i < localStorage.length; i++) {
+                const songId = localStorage.key(i);
+                const iframeHTML = localStorage.getItem(songId);
+
+                const listItem = document.createElement('li');
+                listItem.innerHTML = iframeHTML;
+                favoriteSongsList.appendChild(listItem);
+            }
+        }
+    }
+    if (favoriteSongsList) {
+        updateFavoritesList();
+    }
+});
